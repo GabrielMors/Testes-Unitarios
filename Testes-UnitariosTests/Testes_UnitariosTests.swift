956772f6b4063ef576747ebc1624966718a697fc
Gabriel Mors <gabrielmors210@gmail.com>
2023-10-09 06:22:57 -0400

    criando o primeiro teste

--- Testes-UnitariosTests/Testes_UnitariosTests.swift
+++ Testes-UnitariosTests/Testes_UnitariosTests.swift
@@ -7,39 +7,24 @@
 
 import XCTest
 @testable import Testes_Unitarios
-
+// Aqui você executa todos os testes da classe
+// Para executar todos os testes do projeto. (Command + U)
 final class Testes_UnitariosTests: XCTestCase {
-    // Esta classe contém testes para o código no projeto principal.
-    // Cada função aqui será um teste unitário.
+    
+    var viewController: ViewController!
 
     override func setUpWithError() throws {
-        // Este método é chamado antes da execução de cada método de teste.
-        // Ou seja, o primeiro a ser chamado é ele. (Como se fosse o viewDidLoad)
-        // Pode ser usado para configurar o ambiente de teste, por exemplo, instanciar objetos necessários.
+        viewController = ViewController()
     }
 
     override func tearDownWithError() throws {
-        // Este método é chamado após a execução de cada método de teste.
-        // Imagina ele como o viewDidDisappear, quando ja finalizar o teste
-        // Pode ser usado para realizar limpezas ou liberar recursos utilizados nos testes.
+        viewController = nil
     }
 
     func testExample() throws {
-        // Este é um exemplo de um caso de teste funcional.
-        // Aqui você escreverá as asserções para verificar se o código principal funciona conforme o esperado.
-        // Se algo inesperado acontecer, o teste lançará uma exceção.
-
-        // Exemplo:
-        // XCTAssert(true) // Este é um teste simples. Substitua por suas próprias asserções.
+        var valorTotal = 10 + 10
+        XCTAssertEqual(20, valorTotal)
     }
 
-    func testPerformanceExample() throws {
-        // Este é um exemplo de um teste de desempenho.
-        // Você pode medir o desempenho de uma determinada parte do código.
-        // O tempo de execução da parte dentro do bloco `self.measure` será registrado.
 
-        self.measure {
-            // Coloque o código que você deseja medir aqui.
-        }
-    }
 }
